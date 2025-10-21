348e270b5cbda65cedf9967cd3006531a27b1b71
Godwin Obiomah <obiomahgodwin@gmail.com>
2025-10-21 13:29:04 +0100

    Fix task_5.sql: create customer table and insert row

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,11 @@
--- Insert a single customer into Customers table
-INSERT INTO Customers (customer_id, customer_name, email, address)
+DROP TABLE IF EXISTS customer;
+
+CREATE TABLE IF NOT EXISTS customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
