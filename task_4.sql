6d0c7eb8eb7fdabade6a8038812c8e507bcb0855
Godwin Obiomah <obiomahgodwin@gmail.com>
2025-10-21 12:59:41 +0100

    Add task_4.sql to list full description of Books table

---
+++ task_4.sql
@@ -0,0 +1,11 @@
+-- List full description of the Books table
+SELECT 
+    COLUMN_NAME AS `Column`,
+    COLUMN_TYPE AS `Type`,
+    IS_NULLABLE AS `Nullable`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+  AND TABLE_NAME = 'Books';
